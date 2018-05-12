bf0772f80f4f8e27a8858e6603055d2d54d467df
9antriksh <9antriksh@gmail.com>
2018-05-13 04:31:26 +0530

    Added support for 3d equation all run

--- UserInterface.py
+++ UserInterface.py
@@ -131,3 +131,35 @@ Example 2D data for testing
 <br><br>
 </td></tr></table>
 '''
+
+htmlForm_3D_All = '''
+<table border=1 cellpadding=20>
+<tr><td><b>All 3D Equations</b></td></tr>
+<tr><td>
+<form action="/fitter_3D_All" method="post" target=_blank>
+--- 3D Text Data ---<br>
+<textarea  rows="10" cols="45" name="textdata" wrap=off>
+Example 3D data for testing
+    X      Y       Z
+  3.017  2.175   0.0320
+  2.822  2.624   0.0629
+  1.784  3.144   6.570
+  1.712  3.153   6.721
+  2.972  2.106   0.0313
+  2.719  2.542   0.0643
+  2.0 2.6 4.0  ending text is ignored
+  1.479  2.957   6.583
+  1.387  2.963   6.744
+  2.843  1.984   0.0315
+  2.485  2.320   0.0639
+  0.742  2.568   6.581
+  0.607  2.571   6.753
+</textarea>
+<br><br>
+<input type="textbox" name="count" placeholder="Enter a number">
+<br><br>
+<input type="submit" value="Submit">
+</form>
+<br><br>
+</td></tr></table>
+'''
